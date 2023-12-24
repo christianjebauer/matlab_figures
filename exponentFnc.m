9164a325555784b394e14e16f714366c4e8350b7
Christian Bauer <st143257@stud.uni-stuttgart.de>
2023-12-24 01:18:20 +0100

    export function for IEEE paper

---
+++ exponentFnc.m
@@ -0,0 +1,25 @@
+function exponentFnc(figure, ax, settings, exponentValue)
+%exponentFnc Replaces the exponents in a figure by annotations
+%   The "x" in the exponents is replaced by a dot
+
+    delete(findall(figure,'type','annotation'));
+
+    ax.XAxis.SecondaryLabel.Visible = 'off'; 
+    ax.YAxis.SecondaryLabel.Visible = 'off'; 
+    ax.ZAxis.SecondaryLabel.Visible = 'off'; 
+    
+    tightPos = tightPosition(ax);
+    exponentStartPositions = [tightPos(1,1) + tightPos(1,3), tightPos(1,2);
+                              tightPos(1,1), tightPos(1,2) + tightPos(1,4);
+                              1, 1]; % ToDo
+
+    for index = 1:length(exponentValue)
+        if exponentValue(index) ~= 0
+            annotation('textbox', [exponentStartPositions(index, 1) * settings.expFactor(index,1), ...
+                    exponentStartPositions(index, 2) * settings.expFactor(index,2), 0.1, 0.1], ...
+                'String', ['\cdot10^{', num2str(exponentValue(index)), '}'], 'EdgeColor', 'none', ...
+                'FontName', settings.font, 'FontSize', settings.fontSize);
+        end
+    end
+end
+
---
+++ exportTileIEEE.m
@@ -0,0 +1,74 @@
+%% Exports figure with fullfills the format requirements of an IEEE paper
+function exportTileIEEE(figure, tile, axArray, lgdArray, figure_high, figurePathArray, fileName, varargin)
+
+    % Some general settings
+    try 
+        settingsIEEE.expFactor = cell2mat(varargin{1, 1}{1, 1});
+    catch
+        settingsIEEE.expFactor = [  1, 1;
+                                    0.9, 1;
+                                    1, 1];
+    end
+    settingsIEEE.font = 'Times New Roman';
+    settingsIEEE.fontSize = 8;
+    settingsIEEE.MarkerSize = 3;
+    tile.TileSpacing = 'compact';
+    set(gcf, 'units', 'centimeters', 'position', [0 0 8.63477 figure_high]);
+
+    % Some settings for every axis
+    for ax = axArray
+        ax.XGrid = 'on';
+        ax.YGrid = 'on';
+        ax.ZGrid = 'on';
+        ax.XMinorGrid = 'on';
+        ax.YMinorGrid = 'on';
+        ax.ZMinorGrid = 'on';
+        ax.Box = 'on';
+        ax.BoxStyle = 'full';
+        ax.LabelFontSizeMultiplier = 1;
+        ax.TitleFontSizeMultiplier = 1;
+        ax.FontSizeMode = 'manual';
+        ax.FontSize = settingsIEEE.fontSize;
+        ax.FontName = settingsIEEE.font;
+        try
+            for indexChildren = 1 : length(ax.Children)
+                ax.Children(indexChildren,1).MarkerSize = settingsIEEE.MarkerSize;
+            end
+        end
+    end
+
+    % Adapt the legend accordingly
+    for lgd = lgdArray
+        lgd.FontSize = settingsIEEE.fontSize;
+        lgd.FontName = settingsIEEE.font;
+    end
+
+    % Leave "plot edit" mode
+    plotedit('off');
+
+    % Safe figure with original exponent
+    fileName = erase(fileName, '.pdf');
+    for pathIndex = 1 : length(figurePathArray)
+        path = figurePathArray(pathIndex);
+        savefig(figure, strcat(path, fileName));
+    end
+
+    % Replaces the exponents in a figure by annotations
+    for ax = axArray
+        exponentValue = [ax.XAxis.Exponent; ax.YAxis.Exponent; ax.ZAxis.Exponent];
+        exponentFnc(figure, ax, settingsIEEE, exponentValue)
+    end
+
+    % Prevents undesired cropping of the figure by exportgraphics
+    annotation('rectangle',[0 0 1 1],'Color','w');
+    
+    for pathIndex = 1 : length(figurePathArray)
+        path = figurePathArray(pathIndex);
+        % Export as eps file
+        exportgraphics(gcf, strcat(path, strcat(fileName, '.eps')), 'BackgroundColor', 'none', ...
+            'ContentType', 'vector', 'Resolution', 300);
+        % Export as pdf file
+        exportgraphics(gcf, strcat(path, strcat(fileName, '.pdf')), 'BackgroundColor', 'none', ...
+            'ContentType', 'vector', 'Resolution', 300);
+    end
+end
